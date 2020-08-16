710d35396c7cdf259df2bdbddf22fe1cdbba43f3
Madhushanka12 <mpiyabasitha@gmail.com>
2020-08-17 02:39:46 +0530

    MAD GROUP

---
+++ app/src/main/java/com/example/university_management_system/timetable_update.java
@@ -0,0 +1,14 @@
+package com.example.university_management_system;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class timetable_update extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timetable_update);
+    }
+}
